2a1f0cfc24bca0750283c749781083a0e5ceda60
robertijtsma <robertijtsma@gmail.com>
2025-08-03 07:47:37 +0200

    💥 Full overwrite met lokale versie

--- app/constants.js
+++ app/constants.js
@@ -1,7 +1,7 @@
 const Constants = {
   settings: {
-    appName: 'Leasingrechner',
-    appUrl: 'https://www.leasingrechnen.at', // without trailing slash!
+    appName: 'ElNosso - Dagkaart Gen',
+    appUrl: 'https://qbuzz.elnosso.dev', // without trailing slash!
     nodeIntegrationEnabled: false, // keep disabled unless you run into ipcRenderer-specific troubles
     enableDevMenu: false, // enable for dev-builds only!
     userAgentPostfixWindows: 'WindowsApp', // custom user agent postfixes to distinguish traffic in Analytics
